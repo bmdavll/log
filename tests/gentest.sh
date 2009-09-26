8083c3f19fa239e3dc1b3629736eab6c4774af2b
David Liang <bmdavll@gmail.com>
2009-09-25 20:47:51 -0700

    Updated tests

--- tests/gentest.sh
+++ tests/gentest.sh
@@ -14,12 +14,10 @@ fi
 
 [ $# -ne 0 ] && errorExit
 
-cd "$(dirname "$(readlink -f "$(which "$0")")")" 2>/dev/null || errorExit
-
 declare -a opt
 opt[1]=2 # -d
 opt[2]=2 # -c
-opt[3]=3 # -g -vis
+opt[3]=3 # -g [-v|-G] -is
 opt[4]=3 # -a -f -e
 opt[5]=4 # --first --first-line --last -n --canonical -r -w
 opt[6]=4 # -p -t --list --count
@@ -29,8 +27,15 @@ for i in "${!opt[@]}"; do
     tests=$(( tests * ${opt[i]} ))
 done
 
+exec 1>"$RAND.sh"
+echo '#!/bin/bash'
+echo '# test script for '"$RAND"
+echo
+echo 'set -e'
+echo 'cd "$(dirname "$(readlink -f "$(which "$0")")")" 2>/dev/null'
+echo
+
 for i in $(seq 1 $tests); do
-    echo -n "█◙█ $i"
     opts=()
     for n in $(seq 1 ${#opt[@]}); do
         choices=${opt[$n]}
@@ -40,19 +45,23 @@ for i in $(seq 1 $tests); do
         1)  case $c in
             0)  opts+=(lines.inp)
                 ;;
-            1)  opts+=(-d'>>' delim.inp)
+            1)  opts+=(-d"'>>'" delim.inp)
                 ;;
             esac
             ;;
         2)  case $c in
-            1)  opts+=(-c'#')
+            1)  opts+=(-c"'#'")
                 ;;
             esac
             ;;
         3)  case $c in
-            1)  opts+=(-g'[FB]oo' -g'B[aeiou]T' -i)
+            1)  opts+=(-g"'[FB]oo'" -g"'B[aeiou]T'" -i)
                 ;;
-            2)  opts+=(--grep='[fb]oo' -g 'b[aeiou]t' --not -s)
+            2)  if [ "$RAND" = "rand" ]; then
+                    opts+=(--grep="'[fb]oo'" -g "'b[aeiou]t'" --not -s)
+                else
+                    opts+=(--grep-not="'[fb]oo'" -G "'b[aeiou]t'" -s)
+                fi
                 ;;
             esac
             ;;
@@ -68,9 +77,9 @@ for i in $(seq 1 $tests); do
         5)  case $c in
             1)  opts+=(--first --first-line -n)
                 ;;
-            2)  opts+=(--last -n'.' --canonical -rw)
+            2)  opts+=(--last -n"'.'" --canonical -rw)
                 ;;
-            3)  opts+=(-N'[' -n'] ' --wrap=16)
+            3)  opts+=(-N"'['" -n"'] '" --wrap=16)
                 ;;
             esac
             ;;
@@ -85,8 +94,10 @@ for i in $(seq 1 $tests); do
             ;;
         esac
     done
-    echo "	$RAND ${opts[@]}"
-    $RAND "${opts[@]}" || exit $?
+    echo 'echo "█◙█ '"$i	$RAND ${opts[@]}"'"'
+    echo "$RAND ${opts[@]}"
 done
+
+chmod +x "$RAND.sh"
 
 # vim:set ts=4 sw=4 et:
